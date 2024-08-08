93c507f011f1ab3fef53e0695f0e31b0ba4ebda8
VladislavWaza <vizivrus@gmail.com>
2024-08-08 19:32:23 +0300

    добавил вывод сообщения исключения в main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
+
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    try {
+        MainWindow w;
+        w.show();
+        return a.exec();
+    } catch (const std::exception &e) {
+        QMessageBox::critical(nullptr, "Error", e.what());
+        return EXIT_FAILURE;
+    } catch (...) {
+        QMessageBox::critical(nullptr, "Error", "An unknown error occurred.");
+        return EXIT_FAILURE;
+    }
 }
